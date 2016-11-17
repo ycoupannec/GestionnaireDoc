ffbe3a04557c78adcca923e199c3ab90dd302da7
ycoupannec <yohann.coupannec@gmail.com>
2016-11-17 09:22:39 +0100

    Start project
    Ajout de bootstrap

---
+++ index.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+	  <meta charset="UTF-8"/>
+	  <link href="css/bootstrap.min.css" rel="stylesheet">
+	  <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<!-- <script type="text/javascript" src="js/javas.js"></script> -->
+
+	 
+	</head>
+	<body >
+
+		<!-- <form method="GET" onsubmit="testMail()">
+		    <p>
+		        text : <input type="text" id="text" />
+		       
+		        <input type="submit" name="mMail" id="mail" value="envoyer"  />
+		    </p>
+	    </form> -->
+
+	<script>
+		/*function testMail(){
+			$.get('send-ajax-data.php',{
+				tMail : $('#text').val(),
+
+			})
+		    .done(function(data) {
+		    	
+
+		        alert(data);
+		    })
+		    .fail(function(data) {
+		        alert('Error: ' + data);
+		    });
+		}*/
+	
+	</script>
+	</body>
+</html>
---
+++ send-ajax-data.php
@@ -0,0 +1,11 @@
+<?php
+
+/*	$email = $_REQUEST['tMail'];
+
+	if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+	    echo 'Cet email est correct.';
+	} else {
+	    echo 'Cet email a un format non adapté.';
+	}*/
+
+?>
